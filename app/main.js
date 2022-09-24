8f83258fa1755f098924c77704be458a7eefa3af
badr ghanbiE <bghaE@gmail.com>
2022-09-24 16:28:16 +0100

    add the main functionality for the game

--- app/main.js
+++ app/main.js
@@ -1,6 +1,28 @@
+let choicesArray=Array.from(document.querySelectorAll('.step'));
 let choices=document.querySelectorAll('.step');
+let container=document.querySelector('.steps-container');
+let counterNumber=document.querySelector('.counter__number')
+
+choices.forEach(choice=>choice.addEventListener('click',(e)=>{
+    hideAllChoices();
+    updateClass(choice,false,'hidden');
+    updateClass(container,true,'flexCenter');
+    updateClass(counterNumber.parentElement,false,'hidden');
+    // decreasing the count number
+    setInterval(decreaseCount,1000)
+  }))
 
 
 
-choices.forEach(choice=>choice.addEventListener('click',(e)=>console.log(e.target.classList.contains('steps__paper'))));
+function hideAllChoices(){
+    return  choicesArray.forEach(element=>updateClass(element,true,'hidden'));
+}
+  
+  
+function updateClass(element,isAdd,className){
+   return isAdd ? element.classList.add(`${className}`):element.classList.remove(`${className}`)
+}
 
+function decreaseCount(){
+  if (counterNumber.textContent>0) counterNumber.textContent--;
+}
